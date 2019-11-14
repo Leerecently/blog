7f64c938c5b939ba2d4b3a25b46244f544675d1a
李国祥 <pidli_li@gomro.cn>
2019-11-14 17:19:39 +0800

    blog 初始化

---
+++ src/main/java/com/lee/blog/BlogApplication.java
@@ -0,0 +1,16 @@
+package com.lee.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Lee
+ */
+@SpringBootApplication
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/lee/blog/entity/blog.java
@@ -0,0 +1,10 @@
+package com.lee.blog.entity;
+
+/**
+ * @Author: 李国祥
+ * @Date: 2019/11/14 15:33
+ * @Version 1.0
+ */
+public class blog {
+
+}
